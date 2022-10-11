c0aabfbac02f8a13d2300c990be876412608af5e
tommy <tommyside@centrum.sk>
2022-10-11 13:23:21 +0200

    index stránka, pridavanie hlavneho obsahu, lava strana done

--- index.php
+++ index.php
@@ -3,11 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <title>Strms | Home</title>
+    <link rel="stylesheet" href="style/styleb.css">
+    <script src="https://kit.fontawesome.com/85a6d0cbbe.js" crossorigin="anonymous"></script>
 </head>
 <?php include_once "header.html"; ?>
 <body>
+<div class="container-fluid">
+    <section class="telo">
+        <div class="row">
+            <div class="col-3 text-center bg-danger">
+                <img class="img-thumbnail picture" src="images/questionmark.jpg" alt="Profile picture">
 
-<h1 class="text-center">content bla bla bla</h1>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque commodi dicta in quidem quo sint.</h3>
+                <h4 class="text-secondary">Lorem ipsum dolor sit amet.</h4>
+
+                <section class="info text-start">
+                    <div><i class="fa-solid fa-clock"></i> Lorem ipsum dolor sit amet.</div>
+                    <div><i class="fa-solid fa-coins"></i> Lorem ipsum dolor sit amet, consectetur.</div>
+                    <div><i class="fa-solid fa-globe"></i> Lorem ipsum dolor sit amet, consectetur adipisicing.</div>
+                    <div><i class="fa-solid fa-heart"></i> Lorem ipsum dolor sit amet.</div>
+                </section>
+                <section class="icons">
+                    <a href=""><i class="fa-solid fa-globe fa-2x"></i></a>
+                    <a href=""><i class="fa-brands fa-facebook fa-2x"></i></a>
+                    <a href=""><i class="fa-brands fa-discord fa-2x"></i></a>
+                    <a href=""><i class="fa-brands fa-youtube fa-2x"></i></a>
+                    <a href=""><i class="fa-brands fa-square-instagram fa-2x"></i></a>
+                    <a href=""><i class="fa-brands fa-telegram fa-2x"></i></a>
+                    <a href=""><i class="fa-brands fa-twitter fa-2x"></i></a>
+                </section>
+            </div>
+            <div class="col-9 text-center bg-success">
+
+            </div>
+        </div>
+    </section>
+</div>
+
+
 
 
 </body>
