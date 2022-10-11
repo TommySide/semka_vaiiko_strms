0215b27f4642d2528d406079932e81ee975e0ec1
tommy <tommyside@centrum.sk>
2022-10-11 15:02:47 +0200

    pridanie login/signupform + menšie úpravy

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+    <?php include_once "header.html"; ?>
+<body>
+    <div class="container-fluid">
+
+    </div>
+</body>
+    <?php include_once "footer.html"; ?>
+</html>
