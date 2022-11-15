4cf14abce7a3f5132e0e905d2fd64dbd20baaab5
tommy <tommyside@centrum.sk>
2022-11-16 00:47:46 +0100

    javascript and stuff

--- login.php
+++ login.php
@@ -5,9 +5,11 @@
     <title>Title</title>
     <link rel="stylesheet" href="style/signupforms.css">
     <link rel="stylesheet" href="style/stylegg.css">
+    <script src="https://kit.fontawesome.com/85a6d0cbbe.js" crossorigin="anonymous"></script>
 </head>
     <?php include_once "header.php"; ?>
 <body>
+<script type="application/javascript" src="scripts/script.js"></script>
     <div class="container-fluid">
         <div class="login-page">
             <div class="form text-white">
@@ -15,7 +17,10 @@
                 <br>
                 <form class="login-form" action="includes/login.inc.php" method="post">
                     <input type="text" name="nickname" class="text-white" placeholder="prihlásovacie meno"/>
-                    <input type="password" name="password" class="text-white" placeholder="heslo"/>
+                    <div class="inputicon">
+                        <input type="password" name="password" id="pwd" class="text-white" placeholder="heslo"/>
+                        <i class="fa-regular fa-eye fa-lg" id="icpwd" onclick="showHidePwd()"></i>
+                    </div>
                     <button name="login-submit">prihlasit</button>
                     <p class="message">Nie si registrovaný? <a href="signup.php">Vytvor si účet</a></p>
                     <p class="message">Zabudnuté heslo? <a href="#">Zmeň si ho tu</a></p>
--- signup.php
+++ signup.php
@@ -5,9 +5,11 @@
     <title>Title</title>
     <link rel="stylesheet" href="style/signupforms.css">
     <link rel="stylesheet" href="style/stylegg.css">
+    <script src="https://kit.fontawesome.com/85a6d0cbbe.js" crossorigin="anonymous"></script>
 </head>
 <?php include_once "header.php"; ?>
 <body>
+<script type="application/javascript" src="scripts/script.js"></script>
 <div class="container-fluid">
     <div class="login-page">
         <div class="form text-white">
@@ -16,8 +18,15 @@
             <form class="login-form" action="includes/signup.inc.php" method="post">
                 <input type="text" name="nickname" class="text-white" placeholder="prihlásovacie meno"/>
                 <input type="email" name="email" class="text-white" placeholder="email"/>
-                <input type="password" name="password" class="text-white" placeholder="heslo"/>
-                <input type="password" name="password-rep" class="text-white" placeholder="heslo znova"/>
+                <div class="inputicon">
+                    <input type="password" name="password" id="pwd" onchange="heslaZhoda()" class="text-white" placeholder="heslo"/>
+                    <i class="fa-regular fa-eye fa-lg" id="aa" onclick="showHidePwdReg()"></i>
+                </div>
+                <div class="inputicon">
+                    <input type="password" name="password-rep" onkeyup="heslaZhoda()" id="pwd" class="text-white" placeholder="heslo"/>
+                    <i class="fa-regular fa-eye fa-lg" id="bb" onclick="showHidePwdReg()"></i>
+                </div>
+                <h5 id="hesla"></h5>
                 <button name="login-submit">registruj</button>
                 <p class="message">Už si zaregistrovaný? <a href="login.php">Prihlás sa</a></p>
             </form>
